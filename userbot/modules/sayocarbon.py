5834794d8e821ab3def25b96d7c5f8cdd6ff28c1
Emon135 <95520410+Emon135@users.noreply.github.com>
2021-12-17 03:49:22 +0700

    Update sayocarbon.py

--- userbot/modules/sayocarbon.py
+++ userbot/modules/sayocarbon.py
@@ -79,8 +79,8 @@ async def carbon_api(e):
     await e.client.send_file(
         e.chat_id,
         file,
-        caption="Created by [SAYONARA 😈](https://t.me/Sayonara_nih/)\
-        \nGroup [SUPPORT](https://t.me/NaraXmusic/)",
+        caption="Created by [EMON 😈](https://t.me/berakVbss/)\
+        \nGroup [SUPPORT](https://t.me/greysupport/)",
         force_document=True,
         reply_to=e.message.reply_to_msg_id,
     )
@@ -145,8 +145,8 @@ async def carbon_api(e):
     await e.client.send_file(
         e.chat_id,
         file,
-        caption="created by [SAYONARA 😈](https://t.me/Sayonara_nih/)\
-        \nGroup Support [Dawn Labs](https://t.me/NaraXmusic/)",
+        caption="created by [EMON 😈](https://t.me/berakVbss/)\
+        \nGroup Support [Dawn Labs](https://t.me/greysupport/)",
         force_document=True,
         reply_to=e.message.reply_to_msg_id,
     )
@@ -211,8 +211,8 @@ async def carbon_api(e):
     await e.client.send_file(
         e.chat_id,
         file,
-        caption="Created By [SAYONARA 😈](https://t.me/Sayonara_nih/)\
-        \nGroup Support [Support](https://t.me/NaraXmusic/)",
+        caption="Created By [EMON 😈](https://t.me/berakVbss/)\
+        \nGroup Support [Support](https://t.me/greysupport/)",
         force_document=True,
         reply_to=e.message.reply_to_msg_id,
     )
@@ -277,8 +277,8 @@ async def carbon_api(e):
     await e.client.send_file(
         e.chat_id,
         file,
-        caption="Created by [SAYONARA 😈](https://t.me/Sayonara_nih/),\
-        \nGroup Support [Group Support](https://t.me/naraXmusic)",
+        caption="Created by [EMON 😈](https://t.me/berakVbss/),\
+        \nGroup Support [Group Support](https://t.me/greysupport)",
         force_document=True,
         reply_to=e.message.reply_to_msg_id,
     )
@@ -343,8 +343,8 @@ async def carbon_api(e):
     await e.client.send_file(
         e.chat_id,
         file,
-        caption="Created by [SAYONARA 😈](https://t.me/Sayonara_nih/),\
-        \nGroup Support [Support](https://t.me/NaraXmusic/)",
+        caption="Created by [EMON 😈](https://t.me/berakVbss/),\
+        \nGroup Support [Support](https://t.me/greysupport/)",
         force_document=True,
         reply_to=e.message.reply_to_msg_id,
     )
